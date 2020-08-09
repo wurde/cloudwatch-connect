651aba2dc5e717c4ff37c53359200baea8d4e1f7
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-08-09 01:46:59 -0500

    fmt

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 # Set Terraform variables.
 
 variable "region" {
-    type        = "string"
-    description = "AWS region where the Amazon Connect instance is deployed."
-    default     = "us-east-1"
+  type        = "string"
+  description = "AWS region where the Amazon Connect instance is deployed."
+  default     = "us-east-1"
 }
 
 variable "instanceId" {
-    type        = "string"
-    description = "The Connect instance ID."
-    default     = "2c7326de-916d-2853-9fcf-ac1a631f1326"
+  type        = "string"
+  description = "The Connect instance ID."
+  default     = "2c7326de-916d-2853-9fcf-ac1a631f1326"
 }
 
 variable "queueName" {
-    type        = "string"
-    description = "Name of the primary Amazon Connect Queue."
-    default     = "BasicQueue"
+  type        = "string"
+  description = "Name of the primary Amazon Connect Queue."
+  default     = "BasicQueue"
 }
